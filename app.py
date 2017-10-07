a649690dd409dd35d6dba56adc76dc2d2a60d9e3
Max <maxk893@gmail.com>
2017-10-07 12:41:08 -0700

    Added correct variable to s3Uploader constructor

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ folder_path = config.get("DEFAULT", "folder_path")
 bucket_name = config.get("DEFAULT", "bucket_name")
 s3_path = config.get("DEFAULT", "s3_path")
 
-awsS3 = s3Uploader()
+awsS3 = s3Uploader(s3_path)
 
 for file_name in os.listdir(folder_path):
     awsS3.uploadS3(file_name, folder_path + file_name, bucket_name)
