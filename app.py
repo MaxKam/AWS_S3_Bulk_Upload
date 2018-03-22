a7a598f0581de6091eece0c78f90b2b68a6457e4
Max <maxk893@gmail.com>
2018-03-21 21:37:55 -0700

    Script will now able to upload multiple folders
    Added capability to go through multiple folders and upload items in each folder. Will add parent folder name to file name to have the uploaded files in "folder" in AWS S3.

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ s3_path = config.get("DEFAULT", "s3_path")
 
 awsS3 = s3Uploader(s3_path)
 
-for file_name in os.listdir(folder_path):
-    awsS3.uploadS3(file_name, folder_path + file_name, bucket_name)
+def list_files(dir):
+    file_list = []
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            file_list.append(os.path.join(root, name))
+    return file_list
+
+
+file_list = list_files(folder_path)
+
+for file_item in file_list:
+    file_name = file_item.rsplit("\\", 2) # Gets file name and parent directory from full path
+    file_name = file_name[1] + "/" + file_name[2]
+    awsS3.uploadS3(file_name, file_item, bucket_name)
 
